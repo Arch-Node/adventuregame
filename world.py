75ab519585fefa3636228bd775ec5533a382e01e
Micah <micah.chartier@gmail.com>
2017-12-22 09:05:44 -0700

    Initial Files

---
+++ world.py
@@ -0,0 +1,22 @@
+_world = {}
+starting_position = (0, 0)
+
+
+def load_tiles():
+    """Parse a file that describes the world space into the _world object"""
+    with open('resources/map.csv', 'r') as f:
+        rows = f.readlines()
+    x_max = len(rows[0].split(','))
+    for y in range(len(rows)):
+        cols = rows[y].split(',')
+        for x in range(x_max):
+            tile_name = cols[x].replace('\n', '')
+            if tile_name == 'StartingRoom':
+                global starting_position
+                starting_position = (x, y)
+                #print(starting_position)
+            _world[(x, y)] = None if tile_name == '' else getattr(__import__('tiles'), tile_name)(x, y)
+
+
+def tile_exists(x, y):
+    return _world.get((x, y))
